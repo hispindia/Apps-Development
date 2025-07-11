41378e2d3f21733f47405a2b14d278eca9d0a3fc
Mithilesh Kumar Thakur <mithilesh.hisp@gmail.com>
2025-07-11 15:48:09 +0530

    create app for import aggregated datavalue from XLSX

--- scripts/services.js
+++ scripts/services.js
@@ -5,19 +5,64 @@
 var excelImportAppServices = angular.module('excelImportAppServices', [])
     .service('MetadataService',function(){
        return {
-        //    getRootOrgUnit : function(){
-        //        var def = $.Deferred();
-        //        $.ajax({
-        //            type: "GET",
-        //            dataType: "json",
-        //            contentType: "application/json",
-        //            url: '../../organisationUnits?level=1&fields=id,name',
-        //            success: function (data) {
-        //                def.resolve(data.organisationUnits);
-        //            }
-        //        });
-        //        return def;
-        //    },  
+            getRootOrgUnit : function(){
+                var def = $.Deferred();
+               $.ajax({
+                    type: "GET",
+                    dataType: "json",
+                   contentType: "application/json",
+                    url: '../../organisationUnits?level=1&fields=id,name',
+                    success: function (data) {
+                        def.resolve(data.organisationUnits);
+                    }
+                });
+               return def;
+            },
+           loadOrgUnits : function(id){
+               var def = $.Deferred();
+               var orgUnitMap = [];
+               $.ajax({
+                   type: "GET",
+                   dataType: "json",
+                   async: false,
+                   contentType: "application/json",
+                   //url: '../../organisationUnits/'+id+".json?fields=id,name,programs[id,name,programTrackedEntityAttributes[*],programStages[id,name,programStageDataElements[id,dataElement[id,name],sortOrder]]]",
+                   // url: '../../organisationUnits/'+id+".json?fields=id,name,programs[id,name,programTrackedEntityAttributes[*],programStages[id,name,programStageDataElements[id,dataElement[id,name,optionSet[options[code,displayName]]],sortOrder]]]&paging=false",
+                   url: '../../organisationUnits.json?fields=id,code,name&paging=false',
+                   success: function (orgUnitResponse) {
+                       if (orgUnitResponse.organisationUnits.length !== 0) {
+                           for (var i = 0; i < orgUnitResponse.organisationUnits.length; i++) {
+                               orgUnitMap[orgUnitResponse.organisationUnits[i].code] = orgUnitResponse.organisationUnits[i].id;
+
+                           }
+                       }
+                       def.resolve(orgUnitMap);
+                   }
+               });
+               return def;
+           },
+           loadDataElements : function(){
+               var def = $.Deferred();
+               var dataElementResponseMap = [];
+               $.ajax({
+                   type: "GET",
+                   dataType: "json",
+                   async: false,
+                   contentType: "application/json",
+                   //url: '../../organisationUnits/'+id+".json?fields=id,name,programs[id,name,programTrackedEntityAttributes[*],programStages[id,name,programStageDataElements[id,dataElement[id,name],sortOrder]]]",
+                   // url: '../../organisationUnits/'+id+".json?fields=id,name,programs[id,name,programTrackedEntityAttributes[*],programStages[id,name,programStageDataElements[id,dataElement[id,name,optionSet[options[code,displayName]]],sortOrder]]]&paging=false",
+                   url: '../../dataElements.json?fields=id,code,name,domainType&paging=false&filter=domainType:eq:AGGREGATE',
+                   success: function (dataElementResponse) {
+                       if (dataElementResponse.dataElements.length !== 0) {
+                           for (var i = 0; i < dataElementResponse.dataElements.length; i++) {
+                               dataElementResponseMap[dataElementResponse.dataElements[i].code] = dataElementResponse.dataElements[i].id;
+                           }
+                       }
+                       def.resolve(dataElementResponseMap);
+                   }
+               });
+               return def;
+           }
            
     };
 }); 
